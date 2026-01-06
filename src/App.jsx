25a6195eea04eac60abbd15a60b8cdd8f560596b
Awoyelevictor <awoyeleemma1@gmail.com>
2026-01-06 16:19:47 +0100

    Initial commit from Termux

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { LandingPage } from './ui/LandingPage'
+import Navbar from './components/Home/Navbar' 
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      
+      <Routes>
+        <Route path='/' element={<LandingPage/>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
