7a3d37e5d332e9907e0740becb85657d03adcc95
Awoyelevictor <144042774+Awoyelevictor@users.noreply.github.com>
2026-01-13 13:02:07 +0100

    Update LandingPage.jsx

--- src/ui/LandingPage.jsx
+++ src/ui/LandingPage.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import { About } from '../components/Home/About'
 import Search from '../components/Home/search'
+// Import the new component
+import InputSelector from '../components/Home/InputSelector' 
 
 export const LandingPage = () => {
   return (
-    <div>
+    // Use flex-col and items-center to center everything horizontally
+    <div className="flex flex-col items-center w-full max-w-5xl mx-auto px-4 mt-8">
+      
+      {/* 1. Headline */}
       <About/>
-      <br />
+      
+      {/* 2. New Tabs Section */}
+      <div className="w-full my-4">
+        <InputSelector />
+      </div>
+
+      {/* 3. Search Bar */}
       <Search />
+      
     </div>
   )
 }
